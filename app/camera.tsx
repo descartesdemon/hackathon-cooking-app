aea3261cc785d785d47e748d45bf5cce9e5d4e4e
Kevin Tseng <kevintseng2004@gmail.com>
2024-06-22 12:37:57 -0700

    added camera

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,7 +1,11 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
-export default function Index() {
-  return (
+export default function Camera() {
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions()
+  /*return (
     <View
       style={{
         flex: 1,
@@ -11,5 +15,54 @@ export default function Index() {
     >
       <Text>2</Text>
     </View>
+  );*/
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+  /*function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }*/
+
+  return (
+    <View style={styles.container}>
+      <CameraView style={styles.camera} facing={'back'}>
+      </CameraView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
